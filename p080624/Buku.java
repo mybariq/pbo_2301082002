39c0b33c82301e4734b0e30b234d65b9897d30a0
mybariq <smileprokok@gmail.com>
2024-07-08 20:09:26 +0700

    Add files via upload

---
+++ p080624/Buku.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_2301082002.p080624;
+
+/**
+ *
+ * @author Bariq
+ */
+public class Buku {
+    private String kodeBuku;
+    private String judul;
+    private String pengarang;
+    private String penerbit;
+    private int tahunTerbit;
+  
+    public Buku(){
+    }
+    
+    public Buku(String kodeBuku, String judul, String pengarang, String penerbit, int tahunTerbit){
+        this.kodeBuku = kodeBuku;
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.penerbit = penerbit;
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    public String getKodeBuku() {
+        return kodeBuku;
+    }
+
+    public void setKodeBuku(String kodeBuku) {
+        this.kodeBuku = kodeBuku;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    public void setPengarang(String pengarang) {
+        this.pengarang = pengarang;
+    }
+
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+
+    public int getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public void setTahunTerbit(int tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+}
